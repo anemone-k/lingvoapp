a09f4664c295aa687135fe85cc3a6816f1e8bff1
Anna <midori.maguro@gmail.com>
2021-05-16 22:08:45 +0300

    Added files and changes required for deployment to heroku

--- lingvo/settings.py
+++ lingvo/settings.py
@@ -135,3 +135,8 @@ CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:8080',
 )
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
